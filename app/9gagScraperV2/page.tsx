9d3822ffba3abf7f282bc8b55a67bb6fbebbc7bb
Attila_Tapai <attila.tapai01@gmail.com>
2023-10-03 09:22:50 +0200

    styling update

--- app/9gagScraperV2/page.tsx
+++ app/9gagScraperV2/page.tsx
@@ -1,24 +1,31 @@
 "use client";
 
 import React, { ChangeEvent, useEffect, useState } from "react";
-import { Container, Typography, Button, Select, MenuItem, InputLabel, FormControl, TextField, CircularProgress, SelectChangeEvent, Box } from "@mui/material";
+import { Container, Typography, Button, Select, MenuItem, InputLabel, FormControl, TextField, CircularProgress, Box } from "@mui/material";
 import { Carousel } from "react-responsive-carousel";
+import { SelectChangeEvent } from "@mui/material/Select";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import RootLayout from "../layout";
 import Image from "next/image";
 
-
 const HomePage = () => {
   const [isFetching, setIsFetching] = useState(false);
-  const [maxResults, setMaxResults] = useState(5); // Default value
+  const [maxResults, setMaxResults] = useState(5);
   const [startTime, setStartTime] = useState<number | null>(null);
   const [endTime, setEndTime] = useState<number | null>(null);
   const [showHighlight, setShowHighlight] = useState(false);
   const [highlightTimer, setHighlightTimer] = useState<ReturnType<typeof setInterval> | null>(null);
   const [selectedSection, setSelectedSection] = useState("");
   const [images, setImages] = useState<(string | Blob)[]>([]);
+  const [carouselKey, setCarouselKey] = useState(0);
 
   useEffect(() => {
+    const clearHighlightTimer = () => {
+      if (highlightTimer) {
+        clearInterval(highlightTimer);
+      }
+    };
+
     if (isFetching) {
       const initialTimer = setTimeout(() => {
         setShowHighlight(true);
@@ -26,28 +33,25 @@ const HomePage = () => {
           setInterval(() => {
             setShowHighlight(true);
             setTimeout(() => setShowHighlight(false), 3000);
-          }, 10000) as unknown as ReturnType<typeof setInterval>,
+          }, 10000),
         );
       }, 5000);
 
       return () => {
         clearTimeout(initialTimer);
-        if (highlightTimer !== null) {
-          clearInterval(highlightTimer as unknown as ReturnType<typeof setInterval>);
-        }
+        clearHighlightTimer();
       };
     } else {
       setShowHighlight(false);
-      if (highlightTimer !== null) {
-        clearInterval(highlightTimer as unknown as ReturnType<typeof setInterval>);
-      }
+      clearHighlightTimer();
     }
   }, [isFetching, highlightTimer]);
 
-  const fetchTitles = () => {
+  const fetchTitles = async () => {
     setIsFetching(true);
     setStartTime(Date.now());
     setEndTime(null);
+    setImages([]); // Reset images on new fetch
 
     const requestOptions: RequestInit = {
       method: "GET",
@@ -56,18 +60,17 @@ const HomePage = () => {
       },
     };
 
-    fetch(`/api/9gag?maxResults=${maxResults}&section=${selectedSection}`, requestOptions)
-      .then((res) => res.json())
-      .then((data) => {
-        setImages((prevImages) => [...prevImages, ...data.images]);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      })
-      .finally(() => {
-        setIsFetching(false);
-        setEndTime(Date.now());
-      });
+    try {
+      const res = await fetch(`/api/9gag?maxResults=${maxResults}&section=${selectedSection}`, requestOptions);
+      const data = await res.json();
+      setImages(data.images);
+      setCarouselKey((prevKey) => prevKey + 1);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setIsFetching(false);
+      setEndTime(Date.now());
+    }
   };
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -75,15 +78,18 @@ const HomePage = () => {
     setMaxResults(Math.min(100, Math.max(1, isNaN(val) ? 1 : val)));
   };
 
-  const duration = endTime && startTime ? ((endTime - startTime) / 1000).toFixed(2) : null;
-
   const handleSectionChange = (e: SelectChangeEvent<string>) => {
-    setSelectedSection(e.target.value as string);
+    setSelectedSection(e.target.value);
   };
+
+  const duration = endTime && startTime ? ((endTime - startTime) / 1000).toFixed(2) : null;
 
   return (
     <RootLayout>
-      <Container maxWidth='lg' sx={{ minHeight: '100vh' }}>
+      <Container
+        maxWidth='lg'
+        sx={{ minHeight: "100vh" }}
+      >
         <Box
           pt={4}
           pb={2}
@@ -95,10 +101,13 @@ const HomePage = () => {
             {maxResults} Scraped Screenshot(s) from 9GAG/{selectedSection}
           </Typography>
         </Box>
-        <Box mb={2}>
+        <Box
+          mb={2}
+          sx={{ display: "flex", alignItems: "center" }}
+        >
           <FormControl
             variant='filled'
-            style={{ marginRight: "20px" }}
+            sx={{ marginRight: "20px", width: "200px" }}
           >
             <InputLabel id='section-label'>Section</InputLabel>
             <Select
@@ -106,7 +115,6 @@ const HomePage = () => {
               id='section'
               value={selectedSection}
               onChange={handleSectionChange}
-              sx={{ width: '200px' }}
             >
               <MenuItem value='top'>Top</MenuItem>
               <MenuItem value='trending'>Trending</MenuItem>
@@ -126,9 +134,8 @@ const HomePage = () => {
           </Button>
           <FormControl
             variant='filled'
-            style={{ marginLeft: "20px" }}
+            sx={{ marginLeft: "20px", width: "200px" }}
           >
-            <InputLabel htmlFor='maxResults'></InputLabel>
             <TextField
               type='number'
               id='maxResults'
@@ -137,23 +144,38 @@ const HomePage = () => {
               variant='filled'
               label='Number of results'
               InputProps={{ inputProps: { min: 1, max: 10 } }}
-              sx={{ width: "200px" }}
             />
           </FormControl>
         </Box>
         {duration && <Typography variant='body1'>Fetch duration: {duration} seconds</Typography>}
-        <Carousel>
-          {images.map((base64Image, index) => (
-            <div key={index}>
-              <Image
-                src={`data:image/png;base64,${base64Image}`}
-                alt={`image-${index}`}
-                width={500}
-                height={500}
-              />
-            </div>
-          ))}
-        </Carousel>
+        <div style={{ maxWidth: "800px", margin: "auto" }}>
+          <Carousel
+            key={carouselKey}
+            dynamicHeight={true}
+            emulateTouch={true}
+            showThumbs={false}
+            showStatus={false}
+            showIndicators={false}
+            showArrows={true}
+            infiniteLoop={true}
+            swipeable={true}
+            selectedItem={0}
+          >
+            {images.map((base64Image, index) => (
+              <div
+                key={index}
+                style={{ position: "relative", height: "600px", width: "800px" }}
+              >
+                <Image
+                  src={`data:image/png;base64,${base64Image}`}
+                  alt={`image-${index}`}
+                  layout='fill'
+                  objectFit='cover'
+                />
+              </div>
+            ))}
+          </Carousel>
+        </div>
       </Container>
     </RootLayout>
   );

762dd50cc5817dd054df99221975c50af40cbc7e
Attila_Tapai <attila.tapai01@gmail.com>
2023-10-30 11:53:42 +0100

    working on 9gag scrape

--- app/api/services/9gagScrapeService.ts
+++ app/api/services/9gagScrapeService.ts
@@ -74,55 +74,82 @@ function wait(ms: number): Promise<void> {
 
 export async function capture(maxResults: number, section: string): Promise<Buffer> {
   const url = `https://9gag.com/${section}`;
-  const browser = await puppeteer.launch({ headless: "new" });
+  let browser: puppeteer.Browser | null = null;
 
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1024, height: 768 });
-  await page.goto(url, { waitUntil: "networkidle0" });
+  try {
+    console.log("Launching the browser...");
+    browser = await puppeteer.launch({ headless: true }); // "new" seems to be an invalid value. It should be either true or false.
+    console.log("Browser launched successfully.");
 
-  const clickAcceptButton = async () => {
-    try {
-      const acceptButton = await page.waitForXPath('//button/span[text()="I ACCEPT"]', { timeout: 5000 });
-      await (acceptButton as puppeteer.ElementHandle).click();
-      console.log("Clicked on the cookie acceptance button.");
+    const page = await browser.newPage();
+    console.log("New page created.");
 
-      await wait(1000);
+    await page.setViewport({ width: 1024, height: 768 });
+    console.log("Viewport set.");
 
-      const acceptButtonAgain = await page.$x('//button/span[text()="I ACCEPT"]');
-      if (acceptButtonAgain.length > 0) {
-        await (acceptButtonAgain[0] as puppeteer.ElementHandle).click();
-        console.log("Clicked on the cookie acceptance button again.");
+    console.log(`Navigating to ${url}...`);
+    await page.goto(url, { waitUntil: "networkidle0" });
+    console.log("Navigation complete.");
+
+    const clickAcceptButton = async () => {
+      try {
+        console.log("Waiting for the accept button...");
+        const acceptButton = await page.waitForSelector("#onetrust-accept-btn-handler", { timeout: 5000 });
+        await (acceptButton as puppeteer.ElementHandle).click();
+        console.log("Clicked on the privacy policy acceptance button.");
+
+        await wait(1000);
+
+        const acceptButtonAgain = await page.$("#onetrust-accept-btn-handler");
+        if (acceptButtonAgain) {
+          await (acceptButtonAgain as puppeteer.ElementHandle).click();
+          console.log("Clicked on the privacy policy acceptance button again.");
+        }
+      } catch (error) {
+        console.log("Error while trying to click on the privacy policy acceptance button.", error);
       }
-    } catch (error) {
-      console.log("Error while trying to click on the cookie acceptance button.", error);
+    };
+
+    await clickAcceptButton();
+
+    console.log("Trying to get body handle...");
+    const bodyHandle = await page.$("body");
+    const boundingBox = await bodyHandle?.boundingBox();
+    const height: number = boundingBox?.height || 0;
+    await bodyHandle?.dispose();
+    console.log(`Got body handle. Page height is ${height}px.`);
+
+    const viewportHeight: number = page.viewport()?.height ?? 900;
+    let viewportIncr: number = 0;
+    let numResults: number = 0;
+
+    while (viewportIncr < height && numResults < maxResults) {
+      console.log(`Scrolling the page by ${viewportHeight}px...`);
+      await page.evaluate((_viewportHeight: number) => {
+        window.scrollBy(0, _viewportHeight);
+      }, viewportHeight);
+
+      await wait(3000);
+
+      viewportIncr += viewportHeight;
+      numResults++;
+
+      console.log(`Scrolled ${viewportIncr}px, taken ${numResults} screenshots so far.`);
     }
-  };
 
-  await clickAcceptButton();
+    console.log("Taking a screenshot...");
+    const screenshot = await page.screenshot({ type: "png" });
+    console.log("Screenshot taken.");
 
-  const bodyHandle = await page.$("body");
-  const boundingBox = await bodyHandle?.boundingBox();
-  const height: number = boundingBox?.height || 0;
-  await bodyHandle?.dispose();
-
-  const viewportHeight: number = page.viewport()?.height ?? 900;
-  let viewportIncr: number = 0;
-  let numResults: number = 0;
-
-  while (viewportIncr < height && numResults < maxResults) {
-    await page.evaluate((_viewportHeight: number) => {
-      window.scrollBy(0, _viewportHeight);
-    }, viewportHeight);
-
-    await wait(3000);
-
-    viewportIncr += viewportHeight;
-    numResults++;
-
-    console.log(`Scrolled ${viewportIncr}px, taken ${numResults} screenshots so far.`);
+    return screenshot;
+  } catch (error) {
+    console.log("An unexpected error occurred:", error);
+    throw error; // rethrow the error after logging
+  } finally {
+    if (browser) {
+      console.log("Closing the browser...");
+      await browser.close();
+      console.log("Browser closed.");
+    }
   }
-
-  const screenshot = await page.screenshot({ type: "png" });
-  await browser.close();
-  return screenshot;
 }
